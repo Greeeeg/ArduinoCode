d0b079a907cc95ee609fcbccc82cdc716f9b4da7
Gregory Levinson <gregorylevinson@Gregory-Levinsons-MacBook-Pro.local>
2017-02-05 21:00:05 -0600

    added code

---
+++ justreceivetheshit.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+char receivedSerial;
+
+void setup() 
+{
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
+}
+
+
+void loop() 
+{
+
+mySerial.listen();
+
+   if(mySerial.available())
+    { 
+      char c = mySerial.read();
+      Serial.println("serial received!");
+      delay(500);
+    }
+//    else
+//    {
+//      Serial.println("nah");
+//      delay(500);
+//    }
+//  
+  }//endloop
---
+++ justsendtheshit.ino
@@ -0,0 +1,38 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+int button = 12;
+int buttonState = 0;
+int lastButtonState = 0;
+
+void setup() {
+
+Serial.begin(9600);
+mySerial.begin(9600);
+pinMode(button, INPUT);
+
+}
+ 
+
+void loop() {
+lastButtonState = buttonState;
+buttonState = digitalRead(button);
+
+
+  if (buttonState == HIGH && lastButtonState == LOW) {
+    mySerial.write('c');
+    Serial.println("button push");
+  }
+  
+  
+//  if (buttonState == LOW && lastButtonState == HIGH) {
+//      mySerial.write('d');
+//      Serial.println("button UNpush");
+//  }
+
+}
+
+
+//&& lastButtonState == HIGH
+
+
