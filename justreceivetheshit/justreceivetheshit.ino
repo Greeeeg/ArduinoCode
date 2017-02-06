7fa93eaf656ca581d29e414f86269d44583d1f5e
Samuel B. Sendelbach <s@Sams-MacBook-Pro.local>
2017-02-05 21:15:29 -0600

    Made the code read better

---
+++ justreceivetheshit/justreceivetheshit.ino
@@ -0,0 +1,20 @@
+//change
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+char receivedSerial;
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+
+
+void loop(){
+  mySerial.listen();
+  if(mySerial.available()){ 
+    char c = mySerial.read();
+    Serial.println("serial received!");
+    delay(500);
+    }
+ }
--- justsendtheshit/justsendtheshit.ino
+++ justsendtheshit/justsendtheshit.ino
@@ -6,30 +6,23 @@ int buttonState = 0;
 int lastButtonState = 0;
 
 void setup() {
-
-Serial.begin(9600);
-mySerial.begin(9600);
-pinMode(button, INPUT);
-
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode(button, INPUT);
 }
  
 
 void loop() {
-lastButtonState = buttonState;
-buttonState = digitalRead(button);
-
-
+  lastButtonState = buttonState;
+  buttonState = digitalRead(button);
   if (buttonState == HIGH && lastButtonState == LOW) {
     mySerial.write('c');
     Serial.println("button push");
   }
-  
-  
-//  if (buttonState == LOW && lastButtonState == HIGH) {
-//      mySerial.write('d');
-//      Serial.println("button UNpush");
-//  }
-
+//if (buttonState == LOW && lastButtonState == HIGH) {
+//  mySerial.write('d');
+//  Serial.println("button UNpush");
+//}
 }
 
 
